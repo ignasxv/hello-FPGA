53597d3a23c345f17999cb1f11271df2930d8649
ignasxv <ignasxv@gmail.com>
2025-03-19 07:11:05 -0400

    FEAT: testbench simulation

--- parity-generator/parity_gen_tb.v
+++ parity-generator/parity_gen_tb.v
@@ -3,5 +3,26 @@
 `timescale 1ns/1ps
 
 module test; 
-     
+    reg [6:0] tt_in;
+    wire [7:0] pdata;
+
+    par_gen UUT (
+        .tt_in(tt_in),
+        .pdata(pdata)
+    );
+
+    always #2 tt_in = tt_in + 1;
+        
+    initial begin
+        tt_in = 0;
+
+        $dumpfile("parity_gen_tb.vcd");
+        $dumpvars(0, test);
+        $display("\ntt_in, pdata");
+        
+        $monitor("%b %b", tt_in, pdata);
+        #20 $finish;
+    end 
+
+    
 endmodule
