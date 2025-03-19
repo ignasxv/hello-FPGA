29c340991c003b649c54102baba2b15119dcb9f2
ignasxv <ignasxv@gmail.com>
2025-03-19 08:41:33 -0400

    FEAT: dynamic parity input

--- parity-generator/parity_gen.v
+++ parity-generator/parity_gen.v
@@ -3,7 +3,8 @@
 // Author; Ignas XV
 
 module par_gen (
-    input wire [6:0] tt_in, 
+    input wire [6:0] tt_in,
+    input wire p,    // output parity (0-even, 1-odd) 
     output wire [7:0] pdata
 );
 
